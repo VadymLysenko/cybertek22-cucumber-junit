84f00a86d1de7e20b6375acc8d42eeb6cc2b0f62
VadymLysenko <vesioluy@hotmail.com>
2021-07-15 09:21:40 -0500

    hooks

--- src/test/java/com/cybertek/step_definitions/Hooks.java
+++ src/test/java/com/cybertek/step_definitions/Hooks.java
@@ -2,20 +2,36 @@ package com.cybertek.step_definitions;
 
 import com.cybertek.utilities.Driver;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 
 public class Hooks {
 
     //import io.cucumber.java not from junit
+    @Before(value = "@login", order = 1)
+    public void setupLoginScenario() {
+        System.out.println("---Setting up browser with further details");
+    }
     @Before
     public void setupScenario() {
-        System.out.println("Setting up browser with further details...");
+        System.out.println("---Setting up browser with further details...");
     }
 
     @After
     public void tearDownScenario() {
-        System.out.println("Teardown steps are being applied...");
+        System.out.println("---Teardown steps are being applied...");
         Driver.closeDriver();
+    }
+
+    @BeforeStep
+    public void setupStep() {
+        System.out.println("---setup applying for each step");
+    }
+
+    @AfterStep
+    public void afterStep() {
+        System.out.println("---teardown applying for each step");
     }
 
 }
