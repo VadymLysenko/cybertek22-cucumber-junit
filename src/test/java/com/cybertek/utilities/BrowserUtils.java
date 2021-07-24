a89e2e902a2dacce4ba63082625cef6ee1a38c75
VadymLysenko <vesioluy@hotmail.com>
2021-07-24 11:24:37 -0500

    creating utility method

--- src/test/java/com/cybertek/utilities/BrowserUtils.java
+++ src/test/java/com/cybertek/utilities/BrowserUtils.java
@@ -1,6 +1,34 @@
 package com.cybertek.utilities;
 
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BrowserUtils {
+
+    /** This method accepts List<WebElements> and returns List<String>
+     * @param webElementList
+     */
+    public static List<String> getElementsText(List<WebElement> webElementList) {
+
+        //Create place holder List<String>
+        List<String> actualAsString = new ArrayList<>();
+
+        for (WebElement each : webElementList) {
+
+            actualAsString.add(each.getText());
+
+        }
+        return actualAsString;
+    }
+
+
+
+
+
+
+
     //copy whatever you have from previous project
     public static void sleep(int second)  {
         second*=1000;
