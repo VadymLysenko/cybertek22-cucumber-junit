5059efe30f6e87b189d64c2f1b3e78712c8709e3
VadymLysenko <vesioluy@hotmail.com>
2021-07-24 11:40:59 -0500

    calling utility method from utils class

--- src/test/java/com/cybertek/pages/LibraryLoginPage.java
+++ src/test/java/com/cybertek/pages/LibraryLoginPage.java
@@ -1,5 +1,6 @@
 package com.cybertek.pages;
 
+import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -19,6 +20,37 @@ public class LibraryLoginPage {
 
     @FindBy(xpath = "//button[.='Sign in']")
     public WebElement signIn;
+
+    /*
+    Login method that login with specific username
+     */
+    public void login(){
+        emailInput.sendKeys("username");
+        passwordInput.sendKeys("password");
+        signIn.click();
+    }
+    /*
+    Login with Admin
+     */
+    public void loginWithAdmin(){
+        emailInput.sendKeys("admin");
+        passwordInput.sendKeys("password");
+        signIn.click();
+    }
+    /*
+    Login with ConfigurationReader
+     */
+    public void loginWithConfig(){
+        emailInput.sendKeys(ConfigurationReader.getProperty("librarianUsername"));
+        passwordInput.sendKeys(ConfigurationReader.getProperty("librarianPassword"));
+        signIn.click();
+    }
+
+    public void login(String username, String password){
+        emailInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        signIn.click();
+    }
 
 
 }
