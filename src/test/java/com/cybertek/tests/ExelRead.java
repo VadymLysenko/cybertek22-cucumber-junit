7b3da20c31e8614de5bd05ee28a8870b556edf5e
VadymLysenko <vesioluy@hotmail.com>
2021-07-25 09:48:41 -0500

    day 6 apache poi

---
+++ src/test/java/com/cybertek/tests/ExelRead.java
@@ -0,0 +1,15 @@
+package com.cybertek.tests;
+
+import org.junit.Test;
+
+public class ExelRead {
+
+    /*
+    we will learn how to read from exel file
+    step by step
+     */
+    @Test
+    public void read_from_exel_file(){
+
+    }
+}
