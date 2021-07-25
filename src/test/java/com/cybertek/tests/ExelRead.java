60abad2a5163167f0d525b3b9864db65f6225aa3
VadymLysenko <vesioluy@hotmail.com>
2021-07-25 10:33:03 -0500

    day 6 apache poi

--- src/test/java/com/cybertek/tests/ExelRead.java
+++ src/test/java/com/cybertek/tests/ExelRead.java
@@ -1,6 +1,11 @@
 package com.cybertek.tests;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class ExelRead {
 
@@ -9,7 +14,29 @@ public class ExelRead {
     step by step
      */
     @Test
-    public void read_from_exel_file(){
+    public void read_from_exel_file() throws IOException {
+
+        String path = "SampleData.numbers";
+
+        // to be able to read from exel file,
+        //we need to load in into FileInputStream
+        FileInputStream fileInputStream = new FileInputStream(path);
+
+        // workbook > sheet > row > cell
+
+        // 1 - create new workbook
+        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+
+        // 2 - we need to get specific sheet from currently opened workbook
+        XSSFSheet sheet = workbook.getSheet("Employees");
+
+        // 3 - select row and cell
+        // Print out Steven's cell
+        // Indexes start from 0
+        sheet.getRow(1).getCell(0);
+
+
+
 
     }
 }
